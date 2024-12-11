a3434f389429b765efa214ea52be028a0b61059d
AndyLeq129 <157607030+AndyL129@users.noreply.github.com>
2024-12-11 17:06:40 -0500

    Button Animation

--- music_spaceship/Assets/Scripts/Button.cs
+++ music_spaceship/Assets/Scripts/Button.cs
@@ -57,9 +57,13 @@ public class Button : MonoBehaviour
 
     bool hasStopped = false;
 
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
+
         manager = FindNearestWithTag("manager").GetComponent<instrument_Randomer>();
 
         audio_manager = FindNearestWithTag("manager").GetComponent<Audio_Manager>();
@@ -126,6 +130,7 @@ public class Button : MonoBehaviour
 
     public void OnButtonClicked()
     {
+        anim.SetTrigger("Press");
         manager.detectIfInputListIsFull();
 
         if (this.gameObject.layer == 6)
