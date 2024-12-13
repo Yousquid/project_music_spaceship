eae5e83848b60cdb51f2c4cf92a1322ecd5982c3
Yousquid <ly2456@nyu.edu>
2024-12-13 14:52:03 -0500

    v_1.0

--- music_spaceship/Assets/Scripts/Destination_mark.cs
+++ music_spaceship/Assets/Scripts/Destination_mark.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Destination_mark : MonoBehaviour
 {
     public Destination destination;
+    public GameObject mark;
+    public float timer;
+    public bool DecreaseOrIncrease = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,23 @@ public class Destination_mark : MonoBehaviour
     {
         Quaternion targetRotation = Quaternion.Euler(0, 0, destination.spaceshipRay());
         transform.rotation = targetRotation;
+
+        timer += Time.deltaTime;
+
+        if (timer >= 1f && DecreaseOrIncrease)
+        {
+            mark.SetActive(false);
+            DecreaseOrIncrease = false;
+            timer = 0f;
+        }
+
+
+        if (!DecreaseOrIncrease && timer >= 0.2f)
+        {
+            mark.SetActive(true);
+            DecreaseOrIncrease = true;
+            timer = 0f;
+        }
     }
 
 
